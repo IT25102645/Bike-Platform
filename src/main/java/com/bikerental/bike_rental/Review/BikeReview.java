56f00b282bea68e86f7dc392942141218a3bb4dd
IT25102836 <it25102836@my.sliit.lk>
2026-05-16 21:16:15 +0530

    Update BikeReview class

--- src/main/java/com/bikerental/bike_rental/Review/BikeReview.java
+++ src/main/java/com/bikerental/bike_rental/Review/BikeReview.java
@@ -1,54 +1,75 @@
 package com.bikerental.bike_rental.Review;
 
+// Inheritance - BikeReview extends Review
 public class BikeReview extends Review {
 
+    // Bike-specific fields
     private String bikeId;
     private String bikeName;
     private String bikeCondition;
     private boolean wouldRecommend;
 
+    // Default constructor
     public BikeReview() {
         super();
     }
 
+    // Parameterized constructor
     public BikeReview(String reviewId, String userId, String username,
-                      int rating, String comment,
+                      int rating, String comment, String date,
                       String bikeId, String bikeName,
                       String bikeCondition, boolean wouldRecommend) {
-        super(reviewId, userId, username, rating, comment);
-        this.bikeId = bikeId;
-        this.bikeName = bikeName;
-        this.bikeCondition = bikeCondition;
+        super(reviewId, userId, username, rating, comment, date);
+        this.bikeId         = bikeId;
+        this.bikeName       = bikeName;
+        this.bikeCondition  = bikeCondition;
         this.wouldRecommend = wouldRecommend;
     }
 
+    // Polymorphism - returns review type
     @Override
     public String getReviewType() {
-        return "Bike Review";
+        return "BIKE";
     }
 
+    // Polymorphism - different display format for bike review
     @Override
     public String getDisplaySummary() {
-        return "[Bike: " + bikeName + "] " + getUsername() + " rated " + getRating() + "/5 | "
-                + "Condition: " + bikeCondition + " | "
-                + "Recommend: " + (wouldRecommend ? "Yes" : "No") + " | "
-                + getComment();
+        return "[Bike: " + bikeName + "] " + getUsername() +
+                " rated " + getRating() + "/5 | Condition: " + bikeCondition +
+                " | Recommend: " + (wouldRecommend ? "Yes" : "No") +
+                " | " + getComment();
+    }
+
+    // Getters
+    public String  getBikeId()        { return bikeId; }
+    public String  getBikeName()      { return bikeName; }
+    public String  getBikeCondition() { return bikeCondition; }
+    public boolean isWouldRecommend() { return wouldRecommend; }
+
+    // Setters
+    public void setBikeId(String bikeId)               { this.bikeId         = bikeId; }
+    public void setBikeName(String bikeName)           { this.bikeName       = bikeName; }
+    public void setBikeCondition(String bikeCondition) { this.bikeCondition  = bikeCondition; }
+    public void setWouldRecommend(boolean w)           { this.wouldRecommend = w; }
+
+    // File handling - adds bike fields to parent file string
+    @Override
+    public String toFileString() {
+        return super.toFileString() + "|" + bikeId + "|" + bikeName +
+                "|" + bikeCondition + "|" + wouldRecommend;
     }
 
     @Override
-    public String toCsvLine() {
-        return super.toCsvLine() + "," + bikeId + "," + bikeName + "," + bikeCondition + "," + wouldRecommend;
+    public String toString() {
+        return "BikeReview{" +
+                "reviewId='"    + getReviewId()  + '\'' +
+                ", username='"  + getUsername()  + '\'' +
+                ", rating="     + getRating()    +
+                ", bikeId='"    + bikeId         + '\'' +
+                ", bikeName='"  + bikeName       + '\'' +
+                ", condition='" + bikeCondition  + '\'' +
+                ", recommend="  + wouldRecommend +
+                '}';
     }
-
-    public String getBikeId() { return bikeId; }
-    public void setBikeId(String bikeId) { this.bikeId = bikeId; }
-
-    public String getBikeName() { return bikeName; }
-    public void setBikeName(String bikeName) { this.bikeName = bikeName; }
-
-    public String getBikeCondition() { return bikeCondition; }
-    public void setBikeCondition(String bikeCondition) { this.bikeCondition = bikeCondition; }
-
-    public boolean isWouldRecommend() { return wouldRecommend; }
-    public void setWouldRecommend(boolean wouldRecommend) { this.wouldRecommend = wouldRecommend; }
 }
